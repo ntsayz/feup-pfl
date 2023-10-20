cf1736b73fe7a6657478639bcb410ad68ac8ccea
Ntsay Zacarias <ntsayz@hotmail.com>
2023-10-20 02:23:40 +0100

    fixed the board

--- PRJ1/src/game.pl
+++ PRJ1/src/game.pl
@@ -1,45 +1,10 @@
-initial_board([
-                [out,out,out,sr,sr,sr,sr,sr,out,out],
-                [out,out,out,b_square,b_square,b_square,b_round,b_round,out,out],
-                [out,empty, empty, empty,empty,empty,empty,empty,empty,out],
-                [out,empty, empty, empty,empty,empty,empty,empty,empty,out],
-                [out, out, w_square,w_square,w_square,w_round,w_round,out,out,out],
-                [out,out,sr,sr,sr,sr,sr,out,out,out]]).
-               
-
-display_board(Board) :-
-    initial_board(Board). %  TODO : remove this
-    % TODO : display rows and wtv
-
-
-
-display_board([]).
-display_board([Row|Rows]) :-
-    display_row(Row),
-    display_board(Rows).
-
-display_row([]) :-
-    nl.
-display_row([Cell|Cells]) :-
-    display_cell(Cell),
-    display_row(Cells).
-
-
-display_cell(Cell) :-
-    cell_char(Cell, Char),
-    write(Char),
-    write(' ').
-
-cell_char(ws, 'WS').
-cell_char(sa, 'SA').
-cell_char(empty, 'E').
-cell_char(null, 'N').
+:-use_module(library(lists)).
+:-use_module(library(random)).
 
 start_game :-
     initial_board(Board),
     display_board(Board),
     game_loop(Board, p1).
-
 
 game_loop(Board, CurrentPlayer) :-
     get_single_char(Choice),
@@ -55,7 +20,6 @@ game_loop(Board, CurrentPlayer) :-
 
     % Switch to the next player turn
     game_loop(Board, CurrentPlayer).
-
 
 
 % predicate to switch players
