b0e351004046bae0c5e9689352cb7cbce023794a
Ntsay Zacarias <ntsayyzacarias@gmail.com>
2023-10-31 20:05:52 +0000

    input working

--- PRJ1/src/game.pl
+++ PRJ1/src/game.pl
@@ -6,11 +6,13 @@ start_game :-
     display_board(Board),
     game_loop(Board, p1).
 
+
+
 game_loop(Board, CurrentPlayer) :-
-    get_single_char(Choice),
-    (Choice = 'q' ; Choice = 'Q') ->
-        nl
-    ;
+    
+    read_user_input(Move),
+    
+
     % TODO: update board.
     clear_screen,
     % display board.
@@ -25,3 +27,20 @@ game_loop(Board, CurrentPlayer) :-
 % predicate to switch players
 switch_turn(p1, p2).
 switch_turn(p2, p1).
+
+
+
+read_user_input(Move):-
+    read(Input),    %d4d5.
+    atom_chars(Input, InputList),
+    parse_input(InputList, Move, ListOfMoves).
+
+% predicate to parse user input sictus
+parse_input([X1, Y1, X2, Y2], Move, ListOfMoves) :-
+    char_code(X1, X1Code),
+    char_code(Y1, Y1Code),
+    char_code(X2, X2Code),
+    char_code(Y2, Y2Code).
+    %Move = valid_move(), If valid move returns true ; update board 
+    %ListOfMoves = [Move]
+    
